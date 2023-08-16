5a84d17385ac736090128001e8a56ed3a21916fe
Julio Formiga <julio.formiga@gmail.com>
2023-08-16 09:26:28 +0200

    Chg: lint

--- main.py
+++ main.py
@@ -9,21 +9,26 @@ n = 50000000
 
 
 def calc_c(n=1):
-    print(f"{nbody_c(1):.9f}")
-    print(f"{nbody_c(n):.9f}")
+    print(f'{nbody_c(1):.9f}')
+    print(f'{nbody_c(n):.9f}')
 
 
 def calc_python(n=1):
-    print(f"{nbody_python(1):.9f}")
-    print(f"{nbody_python(n):.9f}")
+    print(f'{nbody_python(1):.9f}')
+    print(f'{nbody_python(n):.9f}')
 
 
-print(f"N-Body - Python {platform.python_version()} with C module")
-print("Time:", timeit(f"calc_c({n})", number=1, setup="from __main__ import calc_c"))
+print(f'N-Body - Python {platform.python_version()} with C module')
+print(
+    'Time:',
+    timeit(f'calc_c({n})', number=1, setup='from __main__ import calc_c'),
+)
 print()
 
-print(f"N-Body - Python {platform.python_version()}")
+print(f'N-Body - Python {platform.python_version()}')
 print(
-    "Time:",
-    timeit(f"calc_python({n})", number=1, setup="from __main__ import calc_python"),
+    'Time:',
+    timeit(
+        f'calc_python({n})', number=1, setup='from __main__ import calc_python'
+    ),
 )
